a447aa4ebb298f2d6f7d10fca35f1c748939555b
Vidar Håland <vusvol@icloud.com>
2021-03-14 21:30:11 +0100

    Feature: Added hamburger to header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
-import { header } from "../styles/header.module.css"
+import * as styles from "../styles/header.module.css"
 
 export default function Header() {
   return (
-    <header className={header}>
+    <header className={styles.header}>
       <Link to="/">
         <img src="/logo_white.svg" alt="Vidar Håland logo" />
       </Link>
+      <div className={styles.hamburger}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
       <p>
         Frontend-utviklar <br />& redaksjonell designer
       </p>
