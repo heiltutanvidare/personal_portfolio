73a696d6a8b4e25658d9d238a1468eca766890f2
heiltutanvidare <vusvol@icloud.com>
2021-03-17 10:07:42 +0100

    Feature: Add GSAP animation to hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,25 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { gsap } from "gsap"
 import * as heroStyles from "../styles/hero.module.css"
 
 export default function Hero({ children }) {
+  // GSAP Animations
+  useEffect(() => {
+    gsap.fromTo(
+      "h1",
+      {
+        opacity: 0,
+        y: "100px",
+        rotate: "7deg",
+      },
+      {
+        opacity: 1,
+        y: "0px",
+        rotate: "0deg",
+        delay: 1,
+      }
+    )
+  }, [])
+
   return <section className={heroStyles.hero}>{children}</section>
 }
